343b5c18ddd7ed49fc0c6acddb2373d15718061d
sushantgadyal19@gmail.com <sushantgadyal19@gmail.com>
2025-06-08 13:23:14 +0530

    feat(middleware): add parameterized rateLimiter middleware and apply to routes
    Created a basic rateLimiter middleware that accepts a window and allowed hits as parameters.
    Integrated the middleware in all main routes with different configurations for each.

---
+++ middlewares/rateLimiter.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const rateLimiter = ({secondsWindow , allowedHits}) => {
+    return function(req,res,next){
+        console.log(`${secondsWindow}  +  ${allowedHits}`);
+        next();
+    }
+
+}
+
+export default rateLimiter;
--- routes/routes.js
+++ routes/routes.js
@@ -1,28 +1,30 @@
 import {Router } from "express";
 const router = Router();
 
-router.get("/", (req,res)=>{
+import rateLimiter from "../middlewares/rateLimiter.js";
+
+router.get("/", rateLimiter( {secondsWindow : 60, allowedHits : 10}) , (req , res) => {
     res.json({
         message : "success",
         from : "home page",
     })
 })
 
-router.get("/api1", (req,res)=>{
+router.get("/api1", rateLimiter( { secondsWindow : 60, allowedHits:10}) , (req , res) => {
     res.json({
         message : "success",
         from : "api-1",
     })
 })
 
-router.get("/api2", (req,res)=>{
+router.get("/api2", rateLimiter({ secondsWindow : 120,  allowedHits : 20}) , (req , res) => {
     res.json({
         message : "success",
         from : "api-2",
     })
 })
 
-router.get("/api3", (req,res)=>{
+router.get("/api3", rateLimiter( { secondsWindow : 120, allowedHits : 30 } ) , (req , res) => {
     res.json({
         message : "success",
         from : "api-3",
