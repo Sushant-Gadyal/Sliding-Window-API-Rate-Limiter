49b84cf1eacbba4620a95575b310fa4fc6a44ead
sushantgadyal19@gmail.com <sushantgadyal19@gmail.com>
2025-06-08 22:11:20 +0530

    feat(route): add /test route for rate limiter testing

--- routes/routes.js
+++ routes/routes.js
@@ -11,25 +11,30 @@ router.get("/", rateLimiter({ secondsWindow: 60, allowedHits: 10 }), (req, res) 
     })
 })
 
-router.get("/api1", rateLimiter({ secondsWindow: 20, allowedHits:5}), (req, res) => {
+router.get("/api1", rateLimiter({ secondsWindow: 20, allowedHits:5 }), (req, res) => {
     res.json({
         message: "success",
         from: "api-1",
     })
 })
 
-router.get("/api2", rateLimiter({ secondsWindow : 120,  allowedHits : 20 }), (req, res) => {
+router.get("/api2", rateLimiter({ secondsWindow: 120,  allowedHits: 20 }), (req, res) => {
     res.json({
         message: "success",
         from: "api-2",
     })
 })
 
-router.get("/api3", rateLimiter({ secondsWindow : 120, allowedHits : 30 }), (req, res) => {
+router.get("/api3", rateLimiter({ secondsWindow: 120, allowedHits: 30 }), (req, res) => {
     res.json({
         message: "success",
         from: "api-3",
     })
 })
 
+router.get("/test", rateLimiter({ secondsWindow:10, allowedHits: 5 }), (req, res) => {
+    res.send("You are allowed");
+})
+
 export default router;
+
