2b93ad969d505ea75f6ae8c7892ac1b49cc784e4
sushantgadyal19@gmail.com <sushantgadyal19@gmail.com>
2025-06-08 15:01:41 +0530

    feat(redis): connect app to Redis Cloud Instance

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import 'dotenv/config'; // Load env variables at the very start
+
 import router from "./routes/routes.js"
+import redis from "./config/redisClient.js";
+
 
 const app = express();
-const PORT = 8050;
+
 
 app.use("/", router);
 
@@ -10,6 +14,6 @@ app.get("/server", (req,res) => {
     res.send("Hello");
 })
 
-app.listen(PORT, () => {
-    console.log(`Server running on PORT http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on PORT http://localhost:${process.env.PORT}`);
 })
