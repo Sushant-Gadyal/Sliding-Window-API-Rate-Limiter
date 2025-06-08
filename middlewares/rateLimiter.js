5168eaa8ff0805833597af996a50cb8574f02cd5
sushantgadyal19@gmail.com <sushantgadyal19@gmail.com>
2025-06-08 19:29:29 +0530

    feat(redis): create rate limit key for IP if missing

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,8 +1,24 @@
 import express from "express";
+import redis from "../config/redisClient.js";
 
 const rateLimiter = ({ secondsWindow , allowedHits }) => {
-    return function(req,res,next){
-        console.log(`${secondsWindow}  +  ${allowedHits}`);
+    return async function(req, res, next){
+
+        const ipAddress = req.ip;
+        const key = `rate_limit:${ipAddress}`;
+
+        const currentSeconds = Math.floor( Date.now() / 1000 ) ;
+
+
+        const exists  = await redis.exists(key);
+        if(!exists){
+            console.log(`${key} does not exists`);
+            const res_add = await redis.zadd(key, currentSeconds, currentSeconds.toString());
+        }
+        else{
+             
+        }
+
         next();
     }
 
